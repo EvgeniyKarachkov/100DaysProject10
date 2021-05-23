10fd174f86e3b22cbe0ab065264ee671969f451d
EvgeniyKarachkov <cazinak341@gmail.com>
2021-05-23 11:46:18 +0300

    Start project

---
+++ 100 Days project 10/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  100 Days project 10
+//
+//  Created by Zodino BLR on 5/23/21.
+//  Copyright © 2021 Zodino BLR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
